ae547481d280a87ae9dea455d7ce802a2416d1e1
Jembut Rontok <rizalmvp@yahoo.com>
2025-08-28 18:37:11 +0700

    setting database

---
+++ database/migrations/2025_08_28_113549_add_username_and_approval_to_users_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('username')->unique()->after('id');
+            $table->boolean('is_admin')->default(false)->after('password');
+            $table->boolean('is_approved')->default(false)->after('is_admin');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn(['username', 'is_admin', 'is_approved']);
+        });
+    }
+};
